b932b8ac84cb26ebedc9a49c152acaa25cecf149
sotomita <so.tomita35135@gmail.com>
2025-04-26 01:36:47 +0900

    add 2D trajectory map

--- bsod/plots/trajectory.py
+++ bsod/plots/trajectory.py
@@ -3,12 +3,54 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import matplotlib.ticker as mticker
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
 
-def plot_trajectory_2d(df_dict: dict, plot_area: list, fig_path: str) -> None:
+def plot_trajectory_2d(
+    df_dict: dict, var_name: str, plot_area: list, fig_path: str, **kwargs
+) -> None:
+    """
+    _summary_
+
+    Parameters
+    ----------
+    df_dict : dict
+        _description_
+    plot_area : list
+        _description_
+    fig_path : str
+        _description_
+    """
+    central_longitude = kwargs.get("central_longitude", 135)
+    if "cmap" in kwargs:
+        cmap = kwargs.get("cmap")
+    else:
+        if var_name == "height":
+            cmap = "viridis"
+        elif var_name == "temp":
+            cmap = "turbo"
+        elif var_name == "rh":
+            cmap = "rainbow_r"
+    if "cbar_ticks" in kwargs:
+        cbar_ticks = kwargs.get("cbar_ticks")
+    else:
+        if var_name == "height":
+            cbar_ticks = np.arange(0, 16001, 2000)
+            vmin = 0
+            vmax = 16000
+        elif var_name == "temp":
+            cbar_ticks = np.arange(-50, 41, 10)
+            vmin = -50
+            vmax = 41
+        elif var_name == "rh":
+            cbar_ticks = np.arange(0, 101, 10)
+            vmin = 0
+            vmax = 100
+
+    colors = cm.Set1(np.arange(len(df_dict)))
 
     fig = plt.figure(figsize=(9, 9))
     ax = fig.add_subplot(
@@ -21,24 +63,32 @@ def plot_trajectory_2d(df_dict: dict, plot_area: list, fig_path: str) -> None:
             max_latitude=plot_area[3],
         ),
     )
+
     ax.set_extent(
         [
             plot_area[0] - 1e-2,
             plot_area[1] + 1e-2,
-            plot_area[2] - 1e-2,
+            plot_area[2] - 5e-2,
             plot_area[3] + 1e-2,
         ]
     )
 
+    # x axis
     ax.set_xlabel("Londitude", fontsize=8)
+
+    # y axis
     ax.set_ylabel("Latitude", fontsize=8)
 
-    # grid lines
+    # grid lines and ticks
     gl = ax.gridlines(
         crs=ccrs.PlateCarree(), color="gray", linestyle="--", draw_labels=True
     )
-    gl.xlocator = mticker.FixedLocator(np.arange(100, 160, 0.5))
-    gl.ylocator = mticker.FixedLocator(np.arange(20, 60, 0.5))
+    if "lon_ticks" in kwargs:
+        lon_ticks = kwargs.get("lon_ticks")
+    gl.xlocator = mticker.FixedLocator(lon_ticks)
+    if "lat_ticks" in kwargs:
+        lat_ticks = kwargs.get("lat_ticks")
+    gl.ylocator = mticker.FixedLocator(lat_ticks)
     gl.top_labels = False
     gl.right_labels = False
 
@@ -46,4 +96,65 @@ def plot_trajectory_2d(df_dict: dict, plot_area: list, fig_path: str) -> None:
     ax.add_feature(cfeature.COASTLINE, linewidth=0.5, linestyle="-")
     ax.coastlines(color="k", linestyle="-")
 
+    st_name_list = list(df_dict.keys())
+    df_list = list(df_dict.values())
+    for i in range(len(st_name_list)):
+        st_name = st_name_list[i]
+        df = df_list[i]
+        lon = df["GeodetLon"].values
+        lat = df["GeodetLat"].values
+        if var_name == "height":
+            var = df["Height"].values
+        elif var_name == "temp":
+            var = df["Temp0"].values
+        elif var_name == "rh":
+            var = df["Humi0"].values
+
+        sc = ax.scatter(
+            lon,
+            lat,
+            c=var,
+            transform=ccrs.PlateCarree(),
+            cmap=cmap,
+            vmin=vmin,
+            vmax=vmax,
+            s=10,
+            zorder=1,
+        )
+
+        ax.scatter(
+            lon[0],
+            lat[0],
+            s=80,
+            transform=ccrs.PlateCarree(),
+            marker="*",
+            color=colors[i],
+            label=st_name,
+            zorder=2,
+        )
+        ax.scatter(
+            lon[-1],
+            lat[-1],
+            s=100,
+            transform=ccrs.PlateCarree(),
+            marker="x",
+            color=colors[i],
+            zorder=2,
+        )
+
+    cbar = plt.colorbar(sc, ax=ax, orientation="horizontal")
+    cbar.set_ticks(cbar_ticks)
+    if var_name == "Height":
+        cbar_label = "Height [m]"
+    elif var_name == "temp":
+        cbar_label = "Temperature [degC]"
+    elif var_name == "rh":
+        cbar_label = "RH [%]"
+    else:
+        cbar_label = ""
+    cbar.set_label(cbar_label)
+
+    ax.legend()
+
+    plt.tight_layout()
     plt.savefig(fig_path, dpi=512)
--- sample/script/figures.py
+++ sample/script/figures.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 
 sys.path.append("../../")
 
@@ -19,23 +20,43 @@ os.makedirs(fig_dir, exist_ok=True)
 fbook = bsod.get_fieldbook(fpath)
 
 
+# plot emagram
+print("*************************************")
+print("emagram")
+print("St. name\tJST time\tsonde No.")
 for i in range(len(fbook)):
     st_name = fbook["st_name"].iloc[i]
     launch_time = fbook["JSTtime"].iloc[i]
     sonde_no = fbook["sonde_no"].iloc[i]
-    print("************************")
-    print(st_name, launch_time, sonde_no)
+    print(f"{st_name}\t{launch_time}\t{sonde_no}")
 
     qcdata_fpath = f"{qc_data_dir}/{st_name}.csv"
     df = pd.read_csv(qcdata_fpath, index_col=0)
 
     # plot temperature emagram
-    os.makedirs(f"{fig_dir}/emagram", exist_ok=True)
-    plot_emagram(st_name, launch_time, df, f"{fig_dir}/emagram/{st_name}.png")
-    """plot_trajectory_2d(
-        {st_name: df},
-        [135.5, 139.5, 33.5, 35.5],
-        f"{fig_dir}/trajectory2d/{st_name}.png",
-    )"""
+    """os.makedirs(f"{fig_dir}/emagram", exist_ok=True)
+    plot_emagram(st_name, launch_time, df, f"{fig_dir}/emagram/{st_name}.png")"""
 
-    break
+# plot 2d trajectory
+df_dict = {}
+for i in range(len(fbook)):
+    st_name = fbook["st_name"].iloc[i]
+    launch_time = fbook["JSTtime"].iloc[i]
+    sonde_no = fbook["sonde_no"].iloc[i]
+    qcdata_fpath = f"{qc_data_dir}/{st_name}.csv"
+    df = pd.read_csv(qcdata_fpath, index_col=0)
+    df_dict[st_name] = df
+
+vars = ["height", "temp", "rh"]
+print("*************************************")
+for var in vars:
+    print(f"trajectory: {var}")
+    os.makedirs(f"{fig_dir}/trajectory2d", exist_ok=True)
+    plot_trajectory_2d(
+        df_dict,
+        var_name=var,
+        plot_area=[135.5, 138.5, 34.0, 35.5],
+        fig_path=f"{fig_dir}/trajectory2d/{var}.png",
+        lon_ticks=np.arange(135.5, 138.6, 0.5),
+        lat_ticks=np.arange(34.0, 35.6, 0.5),
+    )
